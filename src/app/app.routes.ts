ef438b63db6bf060cb632d07e9749b4909206827
Pedro Amaral <amaral.ph.m@gmail.com>
2025-12-10 20:56:54 -0300

    Adding custom favicon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'tickets',
+    path: 'chamados',
     loadComponent: () =>
       import('./features/tickets/tickets.component').then((m) => m.TicketsComponent),
   },
-  { path: '', pathMatch: 'full', redirectTo: 'tickets' },
+  { path: '', pathMatch: 'full', redirectTo: 'chamados' },
   { path: '**', redirectTo: '' },
 ];
